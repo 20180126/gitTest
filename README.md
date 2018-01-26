ad9c3ee59e50a2555473d68f720192393f4ce832
unknown <jnnua7897@gmail.com>
2018-01-26 15:16:14 +0900

    ドキュメントを作成

--- README.md
+++ README.md
@@ -11,6 +11,54 @@
     cd bin
 
     cake migrations migrate
+    
+
+    `
+    app.default.php => app.php
+
+    データベース接続設定、ユーザーとパスを設定
+    
+    `
+
+    'Datasources' => [
+        'default' => [
+            'className' => 'Cake\Database\Connection',
+            'driver' => 'Cake\Database\Driver\Postgres',
+            'persistent' => false,
+            'host' => 'localhost',
+            'username' => 'ユーザ',
+            'password' => 'パス',
+            'database' => '_20180126',
+            'encoding' => 'utf8',
+            'timezone' => 'UTC',
+            'flags' => [],
+            'cacheMetadata' => true,
+            'log' => false,
+
+            /**
+             * Set identifier quoting to true if you are using reserved words or
+             * special characters in your table or column names. Enabling this
+             * setting will result in queries built using the Query Builder having
+             * identifiers quoted when creating SQL. It should be noted that this
+             * decreases performance because each query needs to be traversed and
+             * manipulated before being executed.
+             */
+            'quoteIdentifiers' => false,
+
+            /**
+             * During development, if using MySQL < 5.6, uncommenting the
+             * following line could boost the speed at which schema metadata is
+             * fetched from the database. It can also be set directly with the
+             * mysql configuration directive 'innodb_stats_on_metadata = 0'
+             * which is the recommended value in production environments
+             */
+            //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
+
+            'url' => env('DATABASE_URL', null),
+        ],
+    ],
+
+    cake bake all items
 
 ```
 
---
+++ src/Model/Table/ItemsTable.php
@@ -0,0 +1,70 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Items Model
+ *
+ * @method \App\Model\Entity\Item get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Item newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Item[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Item|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Item patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Item[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Item findOrCreate($search, callable $callback = null, $options = [])
+ */
+class ItemsTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('items');
+        $this->setDisplayField('name');
+        $this->setPrimaryKey('id');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->scalar('name')
+            ->maxLength('name', 255)
+            ->requirePresence('name', 'create')
+            ->notEmpty('name');
+
+        $validator
+            ->scalar('price')
+            ->maxLength('price', 255)
+            ->requirePresence('price', 'create')
+            ->notEmpty('price');
+
+        $validator
+            ->scalar('count')
+            ->maxLength('count', 255)
+            ->requirePresence('count', 'create')
+            ->notEmpty('count');
+
+        return $validator;
+    }
+}
